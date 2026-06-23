a72cc8f551897d1effee526f911ac53e55051137
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-23 23:40:47 +0530

    CORS fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,8 +24,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5176"
+    ENV.CLIENT_URL
   ],
   credentials: true,
 }));
